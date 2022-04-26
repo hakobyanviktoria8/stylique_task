bc0aa16cd8db3008bb66817264ce1553c6e68187
hakobyanviktoria8 <hakobyanviktorya8@gmail.com>
2022-04-26 22:20:11 +0400

    multiply checkbox

--- src/App.js
+++ src/App.js
@@ -3,9 +3,55 @@ import './App.css';
 import done from './images/done.png'
 
 function App() {
-  const[checkedF, setCheckedF]=useState(false)
-  const[checkedDs, setCheckedDs]=useState(false)
-  const[checkedDl, setCheckedDl]=useState(false)
+  const listF = [
+    {
+      id: 1,
+      text: "Foundation 1",
+      check: false
+    },
+    {
+      id: 2,
+      text: "Foundation 2",
+      check: false
+    },
+    {
+      id: 3,
+      text: "Foundation 3",
+      check: false
+    },
+    {
+      id: 4,
+      text: "Foundation 4",
+      check: false
+    },
+    {
+      id: 5,
+      text: "Foundation 5",
+      check: false
+    }
+  ]
+  const listDs = [
+    {
+      id: 1,
+      text: "Discovery 1",
+      check: false
+    },
+    {
+      id: 2,
+      text: "Discovery 2",
+      check: false
+    },
+    {
+      id: 3,
+      text: "Discovery 3",
+      check: false
+    }
+  ]
+  const[listFoundation, setListFoundation]=useState(listF)
+  const[doneFoundation, setDoneFoundation]=useState(false)
+  const[listDiscovery, setListDiscovery]=useState(listDs)
+  const[doneDiscover, setDoneDiscover]=useState(false)
+
   const [data, setData] = useState("");
   
   useEffect(() => {
@@ -18,61 +64,102 @@ function App() {
   }, [])
 
   useEffect(()=>{
-    if(checkedF && checkedDs && checkedDl){
+    listFoundation.map(item=>setDoneFoundation(item.check))
+    listDiscovery.map(item=>setDoneDiscover(item.check))
+    if(doneFoundation && doneDiscover){
       setTimeout(()=>{
         alert(data.text)
       },300)
     }
-  },[checkedF,checkedDs,checkedDl])
+  },[listFoundation, doneFoundation, listDiscovery, doneDiscover])
 
-  const handleChengeFoundation= (e)=>{
-    setCheckedF(e.target.checked= !checkedF);
+  const handleOnChangeF = (id) => {
+    setListFoundation(
+      listFoundation.map(item => {
+        if(item.id === id){
+          return {
+            ...item,
+            check : !item.check
+          }
+        }
+        return item;
+      })
+    )
   }
-  const handleChengeDiscovery= (e)=>{
-    if(checkedF){
-      setCheckedDs(e.target.checked= !checkedDs);
+  const handleOnChangeDs = (id) => {
+    if(doneFoundation){
+      setListDiscovery(
+        listDiscovery.map(item => {
+          if(item.id === id){
+            return {
+              ...item,
+              check : !item.check
+            }
+          }
+          return item;
+        })
+      )
     }
   }
-  const handleChengeDelivery= (e)=>{
-    if(checkedDs){
-      setCheckedDl(e.target.checked= !checkedDl);
-    }
-  }
- 
+
   return (
     <div className='startupStep'>
         <div>
           <h2>
             <span>1</span>
             Foundation
-            <img src={checkedF ? done : ""}/>
+            <img src={doneFoundation ? done : ""}/>
           </h2>
-          <label>
-            <input type="checkbox" checked={checkedF} onChange={handleChengeFoundation}/>
-            Eat
-          </label>
+          {listFoundation.map(item => {
+            return (
+              <div key={item.id}>
+                <label htmlFor={`f-${item.id}`}>
+                  <input
+                    id={`f-${item.id}`}
+                    type="checkbox"
+                    value={item.text}
+                    checked={item.check}
+                    onChange={() => handleOnChangeF(item.id)}
+                  />
+                  {item.text}
+                </label>
+              </div>
+            );
+          })}
         </div>
         <div>
           <h2>
             <span>2</span>
             Discovery
-          <img src={checkedDs ? done : ""}/>
+            <img src={doneDiscover ? done : ""}/>
           </h2>
-          <label>
-            <input type="checkbox" checked={checkedDs} onChange={handleChengeDiscovery}/>
-            Learn
-          </label>
+          {listDiscovery.map(item => {
+            return (
+              <div key={item.id}>
+                <label htmlFor={`ds-${item.id}`}>
+                  <input
+                    id={`ds-${item.id}`}
+                    type="checkbox"
+                    value={item.text}
+                    checked={item.check}
+                    onChange={() => handleOnChangeDs(item.id)}
+                  />
+                  {item.text}
+                </label>
+              </div>
+            );
+          })}
         </div>
         <div>
           <h2>
             <span>3</span>
             Delivery
-          <img src={checkedDl ? done : ""}/>
+            {/* <img src={checkedDl ? done : ""}/> */}
           </h2>
-          <label>
+          {/* <label>
             <input type="checkbox" checked={checkedDl} onChange={handleChengeDelivery}/>
             Learn
-          </label>
+          </label> */}
         </div>
     </div>
   );
